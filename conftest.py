881bff57822d166e28729027d1f832dee88b45b3
Vlad <vladislav.babenya@gmail.com>
2025-12-15 16:19:51 +0300

    added new item test

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
+import requests
 from playwright.sync_api import Playwright, sync_playwright, ViewportSize
 #https://the-internet.herokuapp.com
+
 
 @pytest.fixture
 def driver_hero():
@@ -17,6 +19,7 @@ def driver_hero():
     browser.close()
     playwright.stop()
 
+
 @pytest.fixture
 def driver_demoqa():
     playwright = sync_playwright().start()
@@ -31,3 +34,43 @@ def driver_demoqa():
     context.close()
     browser.close()
     playwright.stop()
+
+
+@pytest.fixture
+def page(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(
+        viewport=ViewportSize(width=1440, height=980),
+        base_url="http://localhost:8080"
+    )
+    page = context.new_page()
+    yield page
+    page.close()
+    context.close()
+    browser.close()
+
+api_token = "11e0fbe05faa66ec576c17ba64859bfa52"
+user_name = "Vlad"
+url = "http://localhost:8080/"
+
+def get_all_jobs():
+    response = requests.get(
+        url=f"{url}api/json",
+        auth=(user_name, api_token)
+    )
+    return response.json()['jobs']
+
+def delete_jobs():
+    jobs_list = get_all_jobs()
+
+    for job in jobs_list:
+        name = job["name"]
+        requests.post(
+            url= f"{url}/job/{name}/doDelete",
+            auth= (user_name, api_token)
+        )
+
+@pytest.fixture(scope="session", autouse=True)
+def delete_jobs_after_all_tests():
+    yield
+    delete_jobs()
---
+++ jenkins_tests/create_new_item.py
@@ -0,0 +1,35 @@
+import random
+import time
+
+def test_tc_01_001_01(page):
+    page.goto("/")
+    username = "Vlad"
+    password = "sifiny99"
+    new_item_name = f"Vlad-{random.randint(0,9999)}"
+
+    username_field_loc = "input[id='j_username']"
+    password_field_loc = "input[id='j_password']"
+    submit_btn_loc = "button[name='Submit']"
+
+    new_item_btn_loc = "a[href='/view/all/newJob']"
+    item_name_field_loc = "input[class='jenkins-input']"
+    item_type_text = "Pipeline"
+    ok_btn_loc = "button[id='ok-button']"
+    logo_loc = "a[class='app-jenkins-logo']"
+    created_item_loc = f"td > a[href='job/{new_item_name}/']"
+
+    page.locator(username_field_loc).fill(username)
+    page.locator(password_field_loc).fill(password)
+    page.locator(submit_btn_loc).click()
+
+    page.locator(new_item_btn_loc).click()  # Клик по кнопке new item
+    page.locator(item_name_field_loc).fill(new_item_name)
+    page.get_by_text(item_type_text, exact=True).click()
+    page.locator(ok_btn_loc).click()
+    page.locator(logo_loc).click()  # Клик по лого
+    text = page.locator(created_item_loc).text_content()  # Получение текста созданного item
+
+    assert text == new_item_name
+
+def test(delete_jobs):
+    assert 1==1
