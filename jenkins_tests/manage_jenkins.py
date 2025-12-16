36b533568d12fd088e1f080d17265097837b3514
Vlad <vladislav.babenya@gmail.com>
2025-12-16 11:38:05 +0300

    added manage jenkins test

---
+++ jenkins_tests/manage_jenkins.py
@@ -0,0 +1,27 @@
+
+
+def test_tc_01_001_01(page):
+    page.goto("/")
+
+    expected_url = "http://localhost:8080/manage/"
+    username = "Vlad"
+    password = "sifiny99"
+
+    username_field_loc = "input[id='j_username']"
+    password_field_loc = "input[id='j_password']"
+    submit_btn_loc = "button[name='Submit']"
+    user_settings_btn_loc = "a[id='root-action-ManageJenkinsAction']"
+
+    page.locator(username_field_loc).fill(username)
+    page.locator(password_field_loc).fill(password)
+    page.locator(submit_btn_loc).click()
+
+    with page.expect_response(lambda response: response.status) as response_info:  # конструкция для перехвата и проверки сетевых ответов при взаимодействии с веб-страницей
+     page.locator(user_settings_btn_loc).click()
+
+    response = response_info.value
+    status_code = response.status
+    print(status_code)
+
+    actual_url = page.url
+    assert actual_url == expected_url
